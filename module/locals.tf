c00047a8dec83889fa0f68d33ef3aa2080287b36
kalis <kalis30nov@yahoo.com>
2023-05-12 11:56:16 +0530

    update

--- module/locals.tf
+++ module/locals.tf
@@ -1,3 +1,3 @@
  locals = {
-    Name = var.env !=  "" ?  "{var.components_name}-${var.env}" : var.components_name
+    name = var.env !=  "" ?  "{var.components_name}-${var.env}" : var.components_name
          }
