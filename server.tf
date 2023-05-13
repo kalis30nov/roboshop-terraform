2fd7e33083053a31883cb34b0247a2444a15eb78
kalis <kalis30nov@yahoo.com>
2023-05-13 09:19:39 +0530

    update

--- server.tf
+++ server.tf
@@ -1,10 +1,10 @@
 module "servers" {
   for_each = var.components
-  
+
   source = "./module"
   env = var.env
   component_name = each.value["name"]
-  component_password = lookup(each.value, "password", "null")
+  #component_password = lookup(each.value, "password", "null")
   instance_type = each.value["instance_type"]
 }
 
