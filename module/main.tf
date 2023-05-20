75586c6330693e086cb1665d1822f0132f38d477
kalis <kalis30nov@yahoo.com>
2023-05-20 09:27:17 +0530

    update

--- module/main.tf
+++ module/main.tf
@@ -40,3 +40,26 @@ resource "aws_route53_record" "dnsroute" {
   ttl      = "300"
   records  = [aws_spot_instance_request.instance.private_ip]
 }
+
+resource "aws_iam_role" "role" {
+  name = "$(var.component_name)-$(var.env)-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+
+  tags = {
+    tag-key = "$(var.component_name)-$(var.env)-role"
+  }
+}
