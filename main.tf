5c7b1d94b8e131a602036f94994174bcfda025d1
kalis <kalis30nov@yahoo.com>
2023-05-27 16:54:39 +0530

    update

--- main.tf
+++ main.tf
@@ -13,5 +13,5 @@ module "app" {
 
   for_each = var.app
   instance_type = each.value["instance_type"]
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  subnet_id = lookup(lookup(module.vpc, "main", null), "subnets", null)
 }
