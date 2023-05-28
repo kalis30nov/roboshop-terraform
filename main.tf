a4631eacb3730d6314d46ff611d89c82aa5da945
kalis <kalis30nov@yahoo.com>
2023-05-28 09:35:20 +0530

    [pp

--- main.tf
+++ main.tf
@@ -13,7 +13,5 @@ module "app" {
 
   for_each = var.app
   instance_type = each.value["instance_type"]
-  subnet_id = lookup(lookup(lookup(module.vpc, "main", null), "subnets", null),each.value["subnet_name"], null)
+  ssubnet_ids     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 }
-
-#lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
